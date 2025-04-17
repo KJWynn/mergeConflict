a68f9c912c3b6cfe292c18e539e4c6f98af93164
Jia Wynn <wynn15185@gmail.com>
2025-04-17 18:18:46 +0800

    test

---
+++ RightBar.tsx
@@ -0,0 +1,27 @@
+import type { Tetromino } from "../constants";
+import { Cell } from "./Cell";
+
+export function RightBar({ nextTetromino, show }: { nextTetromino: Tetromino; show: boolean }) {
+  const matrix: JSX.Element[][] = [];
+  const PREVIEW_GRID_COLS = 6;
+  const PREVIEW_GRID_ROWS = 4;
+
+  for (let i = 0; i < PREVIEW_GRID_ROWS; i += 1) {
+    const row: JSX.Element[] = [];
+    for (let j = 0; j < PREVIEW_GRID_COLS; j += 1) {
+      if (show && nextTetromino.previewBlockPos.some(([x, y]) => x === i && y === j)) {
+        row.push(<Cell key={j} colour={nextTetromino.colour} type="coloured" />);
+      } else {
+        row.push(<Cell key={j} colour="none" />); // game has not started, don't show next tetromino
+      }
+    }
+    matrix.push(row);
+  }
+
+  return (
+    <div className="right-bar">
+      <div className="next-tetromino">Next</div>
+      <div className="next-tetromino-matrix">{matrix}</div>
+    </div>
+  );
+}
